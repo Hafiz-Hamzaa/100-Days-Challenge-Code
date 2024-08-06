9066c5b96aa571856f09fa3c440944c04b23fd71
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 10:15:00 -0700

    Add files via upload

---
+++ day-34/index.js
@@ -0,0 +1,11 @@
+///////------------ Day 34 Coding Challenge------------///////////
+//=================>>>>>>>> Task 100
+var squareRoot = 144;
+squareRoot = Math.sqrt(144);
+console.log(squareRoot);
+//=================>>>>> Task 101
+var random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+//=================>>>>>. Task 102
+var difference = Math.abs(-5 - 5);
+console.log(difference); //======>>> ignoring the sign
---
+++ day-34/index.ts
@@ -0,0 +1,13 @@
+ ///////------------ Day 34 Coding Challenge------------///////////
+//=================>>>>>>>> Task 100
+let squareRoot : number = 144
+squareRoot = Math.sqrt(144)
+console.log(squareRoot);
+
+//=================>>>>> Task 101
+let random : number = Math.floor(Math.random() * 10) + 1
+console.log(random);
+
+//=================>>>>>. Task 102
+const difference : number = Math.abs(-5 - 5)
+console.log(difference); //======>>> ignoring the sign
