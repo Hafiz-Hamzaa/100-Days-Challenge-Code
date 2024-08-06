461352abab678a66f85ca75d11587de11e779194
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 08:24:19 -0700

    Add files via upload

---
+++ day-03/index.ts
@@ -0,0 +1,13 @@
+////////------------------- Day 3 Coding Challenge---------------------///////
+//==========>>> Task 7
+//======>>> Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8
+
+//==========>>> Task 8
+console.log(7 + 1); //====> Addition
+console.log(10 - 2); //=====> Subtraction
+console.log(4 * 2); //=====> Multiplication
+console.log(16 / 2); //======> Division
+
+//==============>>>> Task 9
+let favouriteNumber : number = 6
+console.log(`My favourite number is ${favouriteNumber}`); //=====> Favourite Number
