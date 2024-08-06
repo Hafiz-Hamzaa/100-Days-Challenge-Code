39b0903a8f8f7d7f421c1cee511867d16b155b1f
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 08:18:04 -0700

    Add files via upload

---
+++ day-01/index.js
@@ -0,0 +1,14 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>   100 Days Cdding Challenge Journey   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< //
+// >>>>>>>>>> Coding Objective <<<<<<<<<<<<
+/////////------- Day 1 Coding Challenge --------- //////////
+//=========>>> Task 1 :
+//===========>>>>> Installation ===> ( VS Code , Typescript , Node.JS )
+//==========>>>>> Task 2:
+var personName = "Hamza";
+var messag = "Hello ".concat(personName, " would you like to learn typescript today?");
+console.log(messag);
+//=========>>>> Task 3:
+var userName = "hAmZa";
+console.log(userName.toUpperCase()); //======> UpperCase
+console.log(userName.toLowerCase()); //======> LowerCase
+console.log(userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase()); //=====> TitleCase
---
+++ day-01/index.ts
@@ -0,0 +1,16 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>   100 Days Cdding Challenge Journey   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< //
+// >>>>>>>>>> Coding Objective <<<<<<<<<<<<
+/////////------- Day 1 Coding Challenge --------- //////////
+//=========>>> Task 1 :
+//===========>>>>> Installation ===> ( VS Code , Typescript , Node.JS )
+
+//==========>>>>> Task 2:
+let personName : string = "Hamza"
+let messag: string = `Hello ${personName} would you like to learn typescript today?`
+console.log(messag);
+
+//=========>>>> Task 3:
+let userName : string = "hAmZa"
+console.log(userName.toUpperCase()); //======> UpperCase
+console.log(userName.toLowerCase()); //======> LowerCase
+console.log(userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase()) //=====> TitleCase
