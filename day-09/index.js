28e11e05364daae838918dcbdbcec2514c9a5ac6
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 08:41:50 -0700

    Add files via upload

---
+++ day-09/index.js
@@ -0,0 +1,50 @@
+//////////----------------- Day 9 Coding Challenge-------------//////////////
+//==============>>>>>>>> Task 25
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("player just earned 5 points");
+}
+alien_color = "green";
+if (alien_color == "red") {
+    console.log("no output");
+} //..... no output beacuse the condition is false ......//
+//================>>>>>>>> Task 26
+var alienColor = "green";
+if (alienColor == "green") {
+    console.log("payer just earned 5 points");
+}
+else {
+    console.log("payer just earned 10 points");
+}
+alienColor = "yellow";
+if (alienColor == "green") {
+    console.log("payer just earned 5 points");
+}
+else {
+    console.log("payer just earned 10 points");
+}
+//===================>>>>> Task 27
+var AlienColor = "green";
+//=======>> Green alien color
+if (AlienColor == "green") {
+    console.log("player just earned 5 points");
+}
+else {
+    console.log("player no earned points");
+}
+//======>> Yellow alien color
+AlienColor = "yellow";
+if (AlienColor == "yellow") {
+    console.log("player just earned 10 points");
+}
+else {
+    console.log("player no earned points");
+}
+//=====>> Red alien color
+AlienColor = "red";
+if (AlienColor == "red") {
+    console.log("player just earned 15 points");
+}
+else {
+    console.log("player no earned points");
+}
---
+++ day-09/index.ts
@@ -0,0 +1,48 @@
+//////////----------------- Day 9 Coding Challenge-------------//////////////
+//==============>>>>>>>> Task 25
+let alien_color : string = "green"
+if (alien_color == "green") {
+    console.log("player just earned 5 points");
+}
+alien_color = "green"
+if (alien_color == "red") {
+    console.log("no output");
+} //..... no output beacuse the condition is false ......//
+
+//================>>>>>>>> Task 26
+let alienColor : string = "green"
+if (alienColor == "green") {
+    console.log("payer just earned 5 points");
+}else {
+    console.log("payer just earned 10 points");
+}
+
+alienColor = "yellow"
+if (alienColor == "green") {
+    console.log("payer just earned 5 points");
+}else {
+    console.log("payer just earned 10 points");
+}
+
+//===================>>>>> Task 27
+let AlienColor : string = "green"
+//=======>> Green alien color
+if (AlienColor == "green") {
+    console.log("player just earned 5 points");
+}else {
+    console.log("player no earned points");
+}
+//======>> Yellow alien color
+AlienColor = "yellow"
+if (AlienColor == "yellow") {
+    console.log("player just earned 10 points");
+}else {
+    console.log("player no earned points");
+}
+//=====>> Red alien color
+AlienColor = "red"
+if (AlienColor == "red") {
+    console.log("player just earned 15 points");
+}else {
+    console.log("player no earned points");
+}
