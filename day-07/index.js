1c048ffcba89212c391b8f4d9b744170a9a51eaa
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 08:37:31 -0700

    Add files via upload

---
+++ day-07/index.js
@@ -0,0 +1,15 @@
+//////////------------------- Day 7 Coding Challenge-----------------////////////////
+//===================>>>>>>>> Task 19
+var totalGuest = ["Asma", "Abeeha", "Laiba", "Amna", "Areeba"];
+console.log("I am inviting ".concat(totalGuest.length, " to dinner"));
+//=============>>>> Task 20
+var countries = ["Saudia", "Dubai", "Australia", "Iran"];
+console.log("Countries I'd like to vist", countries);
+//===========>>>>>>> Task 21
+//========>>> Object
+var StudentData = {
+    name: "Ali",
+    rollNo: 1234,
+    selection: "onsite",
+};
+console.log(StudentData);
---
+++ day-07/index.ts
@@ -0,0 +1,17 @@
+//////////------------------- Day 7 Coding Challenge-----------------////////////////
+//===================>>>>>>>> Task 19
+let totalGuest : string[] = ["Asma","Abeeha","Laiba","Amna","Areeba"]
+console.log(`I am inviting ${totalGuest.length} to dinner`);
+
+//=============>>>> Task 20
+let countries: string[] = ["Saudia","Dubai","Australia","Iran"]
+console.log("Countries I'd like to vist" ,countries);
+
+//===========>>>>>>> Task 21
+//========>>> Object
+let StudentData = {
+    name : "Ali",
+    rollNo : 1234,
+    selection : "onsite",
+}
+console.log(StudentData);
