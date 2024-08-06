b1cea6673d09671f467b36abdfc5438d658a08db
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 11:03:38 -0700

    Add files via upload

---
+++ day-50/index.js
@@ -0,0 +1,24 @@
+////------------ Day 50 Coding Challenge-----------////
+//=============>>>>>> Task 148  
+setTimeout(function () {
+    console.log("This message is shown after a 2-second delay.");
+}, 2000);
+// This showcases how to use setTimeout to delay actions in your code.
+//==========>>>>> Task 149
+console.log("Start");
+setTimeout(function () {
+    console.log("Callback executed");
+}, 0);
+console.log("End");
+// Although the timeout is 0, "Callback executed" is logged after "End" due to the event loop.
+//================>>>>> Task 150
+// Synchronous example
+console.log("Before synchronous operation");
+for (var i = 0; i < 1e9; i++) { } // A long loop
+console.log("After synchronous operation");
+// Asynchronous example
+console.log("Before asynchronous operation");
+setTimeout(function () {
+    console.log("Asynchronous operation completed");
+}, 1000);
+console.log("After asynchronous operation setup");
