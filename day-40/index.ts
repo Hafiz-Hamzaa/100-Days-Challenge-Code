8fb2c75181e6cf506d825ad9a9884fc19e034cd5
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 10:31:36 -0700

    Add files via upload

---
+++ day-40/index.ts
@@ -0,0 +1,19 @@
+
+///////------------ Day 40 Coding Challenge------------///////////
+//=================>>>>>>>> Task 118  
+for (let i = 1 ; i <= 10 ; i++) {
+    console.log(i);
+}
+
+//===========>>>>>> Task 119
+let  count : number = 0
+while (count < 5) {
+    console.log("Hello World")
+    count++;
+}
+
+//==============>>>>> Task 120
+let favouriteMovies : string [] = ["Dabang","Train to Busan","Bhootnat"]
+for (let movie of favouriteMovies) {
+    console.log(movie);
+}
