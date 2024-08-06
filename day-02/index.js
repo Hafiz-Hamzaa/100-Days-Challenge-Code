81498609bb1d40b99e6bd41d3069d70a101e16f7
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 08:21:23 -0700

    Add files via upload

---
+++ day-02/index.js
@@ -0,0 +1,13 @@
+///////////------------- Day 2 Coding Challenge -----------------/////////////
+//========>>> Task 4 
+var author = "Albert Einstein";
+var quote = "A person who never made a mistake never tried anything new.";
+console.log("".concat(author, " once said, ").concat(quote)); // Use Template Literals
+//============>>>>> Task 5 
+var famousPerson = "Alber Einstein";
+var message = "A person who never made a mistake never tried anything new.";
+console.log("".concat(famousPerson, " once said, ").concat(message));
+//============>>>>> Task 6
+var person_name = "\n   Hafiz Hamza   \n";
+console.log("Around Whitespaces :", person_name);
+console.log("Original Form :", person_name.trim()); //=====>> .trim() method remove the whitespaces
---
+++ day-02/index.ts
@@ -0,0 +1,15 @@
+///////////------------- Day 2 Coding Challenge -----------------/////////////
+//========>>> Task 4 
+let author : string = "Albert Einstein"
+let quote: string = "A person who never made a mistake never tried anything new."
+console.log(`${author} once said, ${quote}`); // Use Template Literals
+
+//============>>>>> Task 5 
+let famousPerson : string = "Alber Einstein"
+let message : string = "A person who never made a mistake never tried anything new."
+console.log(`${famousPerson} once said, ${message}`);
+
+//============>>>>> Task 6
+let person_name : string = "\n   Hafiz Hamza   \n"
+console.log("Around Whitespaces :", person_name);
+console.log("Original Form :", person_name.trim()); //=====>> .trim() method remove the whitespaces
