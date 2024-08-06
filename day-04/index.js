5b5c85d955f9048d98c07157cfe0ae7f662042c4
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-08-06 08:31:02 -0700

    Add files via upload

---
+++ day-04/index.js
@@ -0,0 +1,19 @@
+//////////////--------------- Day 4 Coding Challenge---------------////////
+//=============>>>> Task 10
+//============>>> Adding Comments
+// userName = Hamza
+// date = 25-04-2024
+//====> The Program prints a favourite transportation 
+var favouriteTransportation = "car";
+console.log("My favourite transportation is ".concat(favouriteTransportation)); // favourite transportation
+//=============>>> Task 11
+var friendNames = ["Ali", "Hamza", "Aswad"];
+console.log(friendNames[0]); //===> Ali
+console.log(friendNames[1]); //===> Hamza
+console.log(friendNames[2]); //====> Aswad
+//=================>>> Task 12
+var names = ["Ayesha", "Amna", "Asma"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hello ".concat(name_1, " would you like to play football."));
+} //=======>> using for loop
---
+++ day-04/index.ts
@@ -0,0 +1,21 @@
+//////////////--------------- Day 4 Coding Challenge---------------////////
+//=============>>>> Task 10
+//============>>> Adding Comments
+// userName = Hamza
+// date = 25-04-2024
+//====> The Program prints a favourite transportation 
+
+let favouriteTransportation : string = "car"
+console.log(`My favourite transportation is ${favouriteTransportation}`); // favourite transportation
+
+//=============>>> Task 11
+let friendNames: string[] = ["Ali", "Hamza", "Aswad"];
+console.log(friendNames[0]); //===> Ali
+console.log(friendNames[1]); //===> Hamza
+console.log(friendNames[2]); //====> Aswad
+
+//=================>>> Task 12
+let names: string[] = ["Ayesha","Amna","Asma"]
+for (let name of names) {
+    console.log(`Hello ${name} would you like to play football.`);
+} //=======>> using for loop
